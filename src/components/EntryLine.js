6a158094d6ff50e0a7acd29b643d5565b613bfd3
Olga <olga142@gmail.com>
2022-05-27 14:00:55 -0400

    Add close button

--- src/components/EntryLine.js
+++ src/components/EntryLine.js
@@ -1,20 +1,26 @@
-import React from 'react'
-import { Grid, Icon, Segment } from 'semantic-ui-react'
+import React, { Fragment, useState } from 'react'
+import {  Grid, Icon, Segment } from 'semantic-ui-react'
+import ModalEdit from './ModalEdit'
 
 function EntryLine( {id, textAlign="right", columns="3", description, value, isExpense=false, deleteEntry}) {
+
+  const [isOpen, setIsOpen] = useState(false)
     return (
+      <Fragment>
         <Segment color={isExpense? 'red' : 'green'}>
         <Grid columns={columns} textAlign={textAlign}>
           <Grid.Row>
             <Grid.Column width={10} textAlign="left">{description}</Grid.Column>
             <Grid.Column width={3} textAlign="right">{value}</Grid.Column>
             <Grid.Column width={3}>
-              <Icon name="edit" bordered/>
+              <Icon name="edit" bordered onClick={()=>setIsOpen(true)}/>
               <Icon name='trash' bordered onClick={() => {deleteEntry(id)}}/>
             </Grid.Column>
           </Grid.Row>
         </Grid>
       </Segment>
+      <ModalEdit isOpen={isOpen} setIsOpen={setIsOpen}></ModalEdit>
+      </Fragment>
     )
 }
 
---
+++ src/components/ModalEdit.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Button, Modal } from 'semantic-ui-react'
+
+function ModalEdit({isOpen, setIsOpen}) {
+    return (
+        <Modal open={isOpen} >
+            <Modal.Header>Edit entry</Modal.Header>
+            <Modal.Content>
+                <Modal.Description>
+                Something here
+                </Modal.Description>
+            </Modal.Content>
+            <Modal.Actions>
+                <Button onClick={()=>setIsOpen(false)}>Close</Button>
+            </Modal.Actions>
+        </Modal>
+    )
+}
+
+export default ModalEdit
