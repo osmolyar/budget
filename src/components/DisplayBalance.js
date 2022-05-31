fe1f091f599923e5216b860e978c0a2b3702f014
Olga <olga142@gmail.com>
2022-05-31 14:01:55 -0400

    format currency

--- src/components/DisplayBalance.js
+++ src/components/DisplayBalance.js
@@ -1,11 +1,31 @@
 import React from 'react'
 import { Statistic } from 'semantic-ui-react'
 
+const defaultOptions = {
+  significantDigits: 2,
+  thousandsSeparator: ',',
+  decimalSeparator: '.',
+  symbol: '$'
+}
+
+const currencyFormatter = (value, options) => {
+  if (typeof value !== 'number') value = 0.0
+  options = { ...defaultOptions, ...options }
+  value = value.toFixed(options.significantDigits)
+
+  const [currency, decimal] = value.split('.')
+  return `${options.symbol} ${currency.replace(
+    /\B(?=(\d{3})+(?!\d))/g,
+    options.thousandsSeparator
+  )}${options.decimalSeparator}${decimal}`
+}
+
+
 function DisplayBalance({size, color, value, label}) {
     return (
      <Statistic size={size} color={color}>
         <Statistic.Label style={{textAlign:"left"}}>{label}</Statistic.Label>
-        <Statistic.Value>{value}</Statistic.Value>
+        <Statistic.Value>{currencyFormatter(value,defaultOptions)}</Statistic.Value>
       </Statistic>
       )
 }
--- src/components/EntryLine.js
+++ src/components/EntryLine.js
@@ -3,13 +3,32 @@ import {  Grid, Icon, Segment } from 'semantic-ui-react'
 
 function EntryLine( {id, textAlign="right", columns="3", description, value, isExpense=false, deleteEntry, editEntry}) {
 
+  const defaultOptions = {
+    significantDigits: 2,
+    thousandsSeparator: ',',
+    decimalSeparator: '.',
+    symbol: '$'
+  }
+  
+  const currencyFormatter = (value, options) => {
+    if (typeof value !== 'number') value = 0.0
+    options = { ...defaultOptions, ...options }
+    value = value.toFixed(options.significantDigits)
+  
+    const [currency, decimal] = value.split('.')
+    return `${options.symbol} ${currency.replace(
+      /\B(?=(\d{3})+(?!\d))/g,
+      options.thousandsSeparator
+    )}${options.decimalSeparator}${decimal}`
+  }
+
     return (
       <Fragment>
         <Segment color={isExpense? 'red' : 'green'}>
         <Grid columns={columns} textAlign={textAlign}>
           <Grid.Row>
             <Grid.Column width={10} textAlign="left">{description}</Grid.Column>
-            <Grid.Column width={3} textAlign="right">{value}</Grid.Column>
+            <Grid.Column width={3} textAlign="right">{currencyFormatter(value,defaultOptions)}</Grid.Column>
             <Grid.Column width={3}>
               <Icon name="edit" bordered onClick={()=>editEntry(id)}/>
               <Icon name='trash' bordered onClick={() => {deleteEntry(id)}}/>
