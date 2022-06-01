06ceb369f3d3c0557defe65c3d81bce9ff17c2bc
Olga <olga142@gmail.com>
2022-06-01 14:44:29 -0400

    dispatch closeEditModal

--- src/components/ModalEdit.js
+++ src/components/ModalEdit.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
 import { Button, Modal } from 'semantic-ui-react'
+import { closeEditModal } from '../actions/modals.actions';
 import EntryForm from './EntryForm'
 
 function ModalEdit({isOpen, setIsOpen, description, value, isExpense, setDescription, setValue, setIsExpense}) {
+
+    const dispatch = useDispatch();
+
     return (
         <Modal open={isOpen} >
             <Modal.Header>Edit entry</Modal.Header>
@@ -17,8 +22,8 @@ function ModalEdit({isOpen, setIsOpen, description, value, isExpense, setDescrip
                   />
             </Modal.Content>
             <Modal.Actions>
-                <Button onClick={()=>setIsOpen(false)}>Close</Button>
-                <Button onClick={() => setIsOpen(false)} primary>Ok</Button>
+                <Button onClick={()=> dispatch(closeEditModal())}>Close</Button>
+                <Button onClick={() => dispatch(closeEditModal())} primary>Ok</Button>
             </Modal.Actions>
         </Modal>
     )
--- src/reducers/modals.reducers.js
+++ src/reducers/modals.reducers.js
@@ -3,6 +3,7 @@ const reducer = (state = {isOpen: false}, action) => {
         case 'OPEN_EDIT_MODAL':
             return {...state, isOpen : true, id : action.payload.id}
         case 'CLOSE_EDIT_MODAL':
+            return {...state, isOpen: false}
         break;
         default:
             return state;
