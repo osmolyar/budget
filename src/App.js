c1df9711e189e03e807c6ead774f9b8101af4af2
Olga <olga142@gmail.com>
2022-06-06 16:38:37 -0400

    Move state and effect hooks into DisplayContainer

--- src/App.js
+++ src/App.js
@@ -1,48 +1,10 @@
-import { useEffect, useState } from 'react';
 import './App.css';
 import DisplayContainer from './components/DisplayContainer';
-import {useDispatch, useSelector} from 'react-redux'
-import { getAllEntries } from './actions/entries.actions';
 
 function App() {
-  
-  const [incomeTotal, setIncomeTotal] = useState(0);
-  const [expenseTotal, setExpenseTotal] = useState(0);
-  const [total, setTotal] = useState();
-  const [entry, setEntry]= useState()
-  const entries = useSelector(state => state.entries)
-  const {isOpen, id}= useSelector(state => state.modals)
-
-  useEffect(() => {
-      const index = entries.findIndex(entry => entry.id === id);
-      setEntry(entries[index])
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[isOpen, id])
-
-  useEffect(() =>
-  {
-    let totalIncomes = 0
-    let totalExpenses=0;
-    entries.map(entry=> {
-      if(entry.isExpense) {
-        return totalExpenses += Number(entry.value)
-      } 
-      return totalIncomes +=Number(entry.value)
-    })
-    setTotal(totalIncomes - totalExpenses)
-    setExpenseTotal(totalExpenses)
-    setIncomeTotal(totalIncomes)
-  },[entries])
-
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getAllEntries())
- }, [dispatch])
 
   return (
-   <DisplayContainer total={total} incomeTotal={incomeTotal} expenseTotal = {expenseTotal} isOpen= {isOpen} entries={entries} entry={entry} />
+   <DisplayContainer  />
     
   );
 }
--- src/components/DisplayContainer.js
+++ src/components/DisplayContainer.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
+import {useDispatch, useSelector} from 'react-redux'
+import { getAllEntries } from '../actions/entries.actions';
 import {  Container} from 'semantic-ui-react';
 import DisplayBalance from './DisplayBalance';
 import DisplayBalances from './DisplayBalances';
@@ -8,7 +11,43 @@ import MainHeader from './MainHeader';
 import ModalEdit from './ModalEdit';
 import NewEntryForm from './NewEntryForm';
 
-function DisplayContainer({total, incomeTotal, expenseTotal, isOpen, entries, entry}) {
+function DisplayContainer() {
+
+  const [incomeTotal, setIncomeTotal] = useState(0);
+  const [expenseTotal, setExpenseTotal] = useState(0);
+  const [total, setTotal] = useState();
+  const [entry, setEntry]= useState()
+  const entries = useSelector(state => state.entries)
+  const {isOpen, id}= useSelector(state => state.modals)
+
+  useEffect(() => {
+      const index = entries.findIndex(entry => entry.id === id);
+      setEntry(entries[index])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[isOpen, id])
+
+  useEffect(() =>
+  {
+    let totalIncomes = 0
+    let totalExpenses=0;
+    entries.map(entry=> {
+      if(entry.isExpense) {
+        return totalExpenses += Number(entry.value)
+      } 
+      return totalIncomes +=Number(entry.value)
+    })
+    setTotal(totalIncomes - totalExpenses)
+    setExpenseTotal(totalExpenses)
+    setIncomeTotal(totalIncomes)
+  },[entries])
+
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllEntries())
+ }, [dispatch])
+
     return (
       <Container>
       <MainHeader title='Budget' />
